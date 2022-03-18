198151f9898a535c4cc66f23e723fdb936f79d58
gusgeneris <gusgeneris92@gmail.com>
2022-03-18 20:20:44 -0300

    Creacion de ApiRestFramework, aplicacion de serializers en las vistas de esta app

--- DjangoApiMySql/DjangoApiMySql/urls.py
+++ DjangoApiMySql/DjangoApiMySql/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/',include('Api.urls'))
+    path('api/',include('Api.urls')),
+    path('apiRest/',include('RestFrameworkApi.urls'))
 ]
